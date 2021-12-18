da3a4307533a8d74d6367ee71c47d09b3c4c23af
mbakin <mehmetberkakinn@outlook.com>
2021-12-18 13:29:39 +0300

    feat: conditional logic

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = new Vue ({
     data: {
         product : "Socks",
         image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inventory: 100,
     }
 })
