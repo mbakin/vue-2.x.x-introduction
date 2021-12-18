b6673fae91cb904b449b79fc02cad0a0e731b659
mbakin <mehmetberkakinn@outlook.com>
2021-12-18 16:45:28 +0300

    create component structure

--- main.js
+++ main.js
@@ -1,45 +1,82 @@
+Vue.component("product", {
+  template: `
+	<div class="product">
+        <div class="product-image">
+          <img :src="image" alt="" />
+        </div>
+
+        <div class="product-info">
+          <h1> {{ title }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else :class="{ outOfStock : !inStock }" >Out of Stock</p>
+
+          <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+          </ul>
+
+          <div
+            v-for="(variant, index) in variants"
+            :key="variant.variantId"
+            class="color-box"
+            :style="{ backgroundColor: variant.variantColor }"
+            @mouseover="updateProduct(index)"
+          ></div>
+
+          <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton : !inStock }">Add to Cart</button>
+
+          <div class="cart">
+            <p>Cart({{ cart }})</p>
+          </div>
+        </div>
+      </div>`,
+			
+			data () {
+				return {
+					brand: "Vue Mastery",
+					product: "Socks",
+					selectedVariant: 0,
+					link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+					onSale: true,
+					details: ["80% cotton", "20% polyester", "Gender-neutral"],
+					variants: [
+						{
+							variantId: 2234,
+							variantColor: "green",
+							variantImage: "./assets/green.jpg",
+							variantQuantity: 10,
+						},
+						{
+							variantId: 2235,
+							variantColor: "blue",
+							variantImage: "./assets/blue.jpg",
+							variantQuantity: 0,
+						},
+					],
+					cart: 0,
+				};
+				},
+				methods: {
+					addToCart() {
+						this.cart += 1;
+					},
+					updateProduct(index) {
+						this.selectedVariant = index;
+					},
+				},
+				computed: {
+					title() {
+						return this.brand + " " + this.product;
+					},
+					image() {
+						return this.variants[this.selectedVariant].variantImage;
+					},
+					inStock() {
+						return this.variants[this.selectedVariant].variantQuantity;
+					},
+				},
+	});
+			
+
 var app = new Vue({
-  el: "#app",
-  data: {
-		brand : "Vue Mastery",
-    product: "Socks",
-    selectedVariant: 0,
-    link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-    onSale: true,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: "green",
-				variantImage: "./assets/green.jpg",
-				variantQuantity: 10
-      },
-      {
-        variantId: 2235,
-        variantColor: "blue",
-				variantImage: "./assets/blue.jpg",
-				variantQuantity: 0
-      },
-    ],
-		cart: 0,
-  },
-	methods: {
-		addToCart() {
-			this.cart += 1
-		},
-		updateProduct(index) {
-			this.selectedVariant = index
-		}
-	},
-	computed: {
-		title() {
-			return this.brand + " " + this.product
-		},
-		image() {
-			return this.variants[this.selectedVariant].variantImage
-		},
-		inStock() {
-			return this.variants[this.selectedVariant].variantQuantity
-		}
-	}
+  el: "#app"
 });
