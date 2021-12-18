e7cf010bd905460ff8d8a2f5782ac3534019068d
mbakin <mehmetberkakinn@outlook.com>
2021-12-18 13:33:39 +0300

    feat: conditional rendering less complex with using `v-show`

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var app = new Vue ({
         product : "Socks",
         image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inventory: 100,
+        inStock: true,
     }
 })
