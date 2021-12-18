87e441ce752c16781fffb99a74fee838c32a7ce9
mbakin <mehmetberkakinn@outlook.com>
2021-12-18 13:22:50 +0300

    feat: attribute binding

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var app = new Vue ({
     el: '#app',
     data: {
-        product : "Socks"
+        product : "Socks",
+        image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 })
